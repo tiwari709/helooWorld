622c7def2012e3761e8ccc3a142d2adef981736e
Rohit <rohittiari709yahoo.com>
2021-11-11 00:02:20 +0530

    hi elo

---
+++ eclipse-workspace/GitHubProject/src/Calc/Addition.java
@@ -0,0 +1,15 @@
+package Calc;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Hi");
+	int a=10;
+	int b=15;
+	
+	System.out.println("Addition of a and b is "+(a+b));
+	
+	
+	}
+
+}
